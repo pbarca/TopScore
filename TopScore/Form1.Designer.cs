2417a850d033d1d4d736f7faebf5ff10df665ce4
Paulo Coto <pbarca@hotmail.com>
2020-03-03 13:00:01 +0000

    fly

--- TopScore/Form1.Designer.cs
+++ TopScore/Form1.Designer.cs
@@ -29,33 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
+            this.clickme = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.record = new System.Windows.Forms.TextBox();
+            this.pontos = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.tempo = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // clickme
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(358, 207);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(182, 56);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Click Mee!!!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.clickme.BackColor = System.Drawing.Color.Transparent;
+            this.clickme.BackgroundImage = global::TopScore.Properties.Resources.fly_2;
+            this.clickme.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.clickme.FlatAppearance.BorderSize = 0;
+            this.clickme.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.clickme.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.clickme.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clickme.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickme.Location = new System.Drawing.Point(477, 255);
+            this.clickme.Margin = new System.Windows.Forms.Padding(4);
+            this.clickme.Name = "clickme";
+            this.clickme.Size = new System.Drawing.Size(100, 100);
+            this.clickme.TabIndex = 0;
+            this.clickme.UseVisualStyleBackColor = false;
+            this.clickme.Click += new System.EventHandler(this.clickme_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(337, 429);
+            this.button2.Location = new System.Drawing.Point(449, 528);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 59);
+            this.button2.Size = new System.Drawing.Size(224, 73);
             this.button2.TabIndex = 1;
             this.button2.Text = "GO!";
             this.button2.UseVisualStyleBackColor = true;
@@ -65,40 +74,44 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(574, 73);
+            this.label1.Location = new System.Drawing.Point(765, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 31);
+            this.label1.Size = new System.Drawing.Size(127, 39);
             this.label1.TabIndex = 2;
             this.label1.Text = "Record";
             this.label1.Click += new System.EventHandler(this.Label1_Click);
             // 
-            // textBox1
+            // record
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(689, 73);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 38);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "56";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.record.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.record.Location = new System.Drawing.Point(919, 90);
+            this.record.Margin = new System.Windows.Forms.Padding(4);
+            this.record.Name = "record";
+            this.record.Size = new System.Drawing.Size(132, 46);
+            this.record.TabIndex = 3;
+            this.record.Text = "0";
+            this.record.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox2
+            // pontos
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(688, 12);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 38);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.Text = "12";
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.pontos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pontos.Location = new System.Drawing.Point(917, 15);
+            this.pontos.Margin = new System.Windows.Forms.Padding(4);
+            this.pontos.Name = "pontos";
+            this.pontos.Size = new System.Drawing.Size(132, 46);
+            this.pontos.TabIndex = 5;
+            this.pontos.Text = "0";
+            this.pontos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(532, 12);
+            this.label2.Location = new System.Drawing.Point(709, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(144, 31);
+            this.label2.Size = new System.Drawing.Size(180, 39);
             this.label2.TabIndex = 4;
             this.label2.Text = "Pontuação";
             // 
@@ -106,20 +119,22 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(53, 17);
+            this.label3.Location = new System.Drawing.Point(71, 21);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 31);
+            this.label3.Size = new System.Drawing.Size(123, 39);
             this.label3.TabIndex = 6;
             this.label3.Text = "Tempo";
             // 
             // tempo
             // 
             this.tempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tempo.Location = new System.Drawing.Point(51, 68);
+            this.tempo.Location = new System.Drawing.Point(68, 84);
+            this.tempo.Margin = new System.Windows.Forms.Padding(4);
             this.tempo.Name = "tempo";
-            this.tempo.Size = new System.Drawing.Size(100, 38);
+            this.tempo.Size = new System.Drawing.Size(132, 46);
             this.tempo.TabIndex = 7;
-            this.tempo.Text = "20";
+            this.tempo.Text = "10";
             this.tempo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // timer1
@@ -128,18 +143,19 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::TopScore.Properties.Resources.d31345678632460cb3b9b3f1eb82d509;
-            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.ClientSize = new System.Drawing.Size(1067, 615);
             this.Controls.Add(this.tempo);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pontos);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.record);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clickme);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -149,11 +165,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button clickme;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox record;
+        private System.Windows.Forms.TextBox pontos;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox tempo;
--- TopScore/Form1.cs
+++ TopScore/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TopScore
@@ -15,6 +9,7 @@ namespace TopScore
         public Form1()
         {
             InitializeComponent();
+            clickme.Visible = false;
         }
 
         private void Label1_Click(object sender, EventArgs e)
@@ -22,17 +17,42 @@ namespace TopScore
 
         }
 
+        private void Start()
+        {
+            button2.Text = "STOP";
+            tempo.Text = "10";
+            clickme.Visible = true;
+            timer1.Start();
+            pontos.Text = "0";
+        }
+
+        private void Stop()
+        {
+            button2.Text = "GO!";
+            clickme.Visible = false;
+            timer1.Stop();
+            if (Convert.ToInt32(record.Text) < Convert.ToInt32(pontos.Text))
+                record.Text = pontos.Text;
+        }
         private void Button2_Click(object sender, EventArgs e)
         {
-            if (button2.Text == "GO!") button2.Text = "STOP";
-            else button2.Text = "GO!";
+            if (button2.Text == "GO!") Start(); else Stop();
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
+            timer1.Interval = 1000;
             int _tempo = Convert.ToInt32(tempo.Text);
-            _tempo--;
+            if (_tempo < 1) Stop(); else _tempo--;
             tempo.Text = _tempo.ToString();
+        }
+
+        private void clickme_Click(object sender, EventArgs e)
+        {
+            pontos.Text = (Convert.ToInt32(pontos.Text) +1).ToString();
+            Random x = new Random();
+            Point pt = new Point(int.Parse(x.Next(500).ToString()),int.Parse(x.Next(250).ToString()) + 100);
+            clickme.Location = pt;
         }
     }
 }
